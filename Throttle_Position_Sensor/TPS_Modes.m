dbec28faa18f63f273dab158b945b46c60f79fd2
Mahmoud Elsaeed <m.elsaeed.02@gmail.com>
2025-03-22 06:34:13 +0200

    Add files via upload

---
+++ Throttle_Position_Sensor/TPS_Modes.m
@@ -0,0 +1,8 @@
+classdef (Enumeration) TPS_Modes< Simulink.IntEnumType
+    enumeration
+        Normal(0)
+        Downgraded_sen1_mode(1)
+        Downgraded_sen2_mode(2)
+        Failure_mode(3)
+    end
+end
